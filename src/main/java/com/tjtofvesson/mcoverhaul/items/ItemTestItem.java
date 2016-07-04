b53f91674b85d60dfdcea4460910a2f11620e860
tjkenmate <tjkenmate@gmail.com>
2016-07-04 11:11:53 -0400

    Add files via upload

--- src/main/java/com/tjtofvesson/mcoverhaul/items/ItemTestItem.java
+++ src/main/java/com/tjtofvesson/mcoverhaul/items/ItemTestItem.java
@@ -1,13 +1,26 @@
 package com.tjtofvesson.mcoverhaul.items;
 
 import com.tjtofvesson.mcoverhaul.Reference;
+
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class ItemTestItem extends Item {
 	
 	public ItemTestItem() {
 		setUnlocalizedName(Reference.MCOItemsData.TESTITEM.getUnlocalizedName());
-		setRegistryName(Reference.MCOItemsData.TESTITEM.getRegistryName());
+		setRegistryName(Reference.MCOItemsData.TESTITEM.getRegistryName());	
+		this.setCreativeTab(CreativeTabs.COMBAT);
 	}
 	
+	/**
+     * gets the CreativeTab this item is displayed on
+     */
+    @SideOnly(Side.CLIENT)
+    public CreativeTabs getCreativeTab()
+    {
+        return CreativeTabs.COMBAT;
+    }
 }
