a4e66af35c6cb0a8da2e9a8924a46e90e92d2bcf
Gabriel Tofvesson <gabriel.tofvesson@hotmail.com>
2016-07-05 23:55:04 +0200

    Changed to SemVer standard. Compiled to jar.

--- src/main/java/com/tjtofvesson/mcoverhaul/Reference.java
+++ src/main/java/com/tjtofvesson/mcoverhaul/Reference.java
@@ -8,7 +8,7 @@ public class Reference {
 
 	public static final String MOD_ID = "ttmco";
 	public static final String NAME = "Minecraft Overhaul";
-	public static final String VERSION = "0.2 Alpha";
+	public static final String VERSION = "0.2.1";
 	public static final String MC_VERS = "[1.9.4,1.10.2]";
 	
 	public static final String CLIENT_PROXY_CLASS = "com.tjtofvesson.mcoverhaul.proxy.ClientProxy";
