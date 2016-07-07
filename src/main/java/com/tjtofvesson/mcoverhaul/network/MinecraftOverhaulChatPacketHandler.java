e29d3ab907b40db683eb734bc4f5f26938958558
tjkenmate <tjkenmate@gmail.com>
2016-07-07 13:02:56 -0400

    Add files via upload

---
+++ src/main/java/com/tjtofvesson/mcoverhaul/network/MinecraftOverhaulChatPacketHandler.java
@@ -0,0 +1,37 @@
+package com.tjtofvesson.network;
+
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraftforge.fml.common.network.NetworkRegistry;
+import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
+import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
+import net.minecraftforge.fml.relauncher.Side;
+import com.tjtofvesson.Reference;
+import com.tjtofvesson.utils.ChatUtil;
+
+public class MinecraftOverhaulChatPacketHandler
+{
+    public static final SimpleNetworkWrapper INSTANCE = new SimpleNetworkWrapper(Reference.MODID);
+
+    public static void init()
+    {
+        INSTANCE.registerMessage(ChatUtil.PacketNoSpamChat.Handler.class, ChatUtil.PacketNoSpamChat.class, 0, Side.CLIENT);
+        INSTANCE.registerMessage(ItemRouterButtonPacketProcessor.class, ItemRouterButtonPacketProcessor.class, 1, Side.SERVER);
+        INSTANCE.registerMessage(PacketSyncConfig.class, PacketSyncConfig.class, 2, Side.CLIENT);
+    }
+
+    public static void sendToAllAround(IMessage message, TileEntity te, int range)
+    {
+        INSTANCE.sendToAllAround(message, new NetworkRegistry.TargetPoint(te.getWorld().provider.getDimensionId(), te.getPos().getX(), te.getPos().getY(), te.getPos().getZ(), range));
+    }
+
+    public static void sendToAllAround(IMessage message, TileEntity te)
+    {
+        sendToAllAround(message, te, 64);
+    }
+
+    public static void sendTo(IMessage message, EntityPlayerMP player)
+    {
+        INSTANCE.sendTo(message, player);
+    }
+}
